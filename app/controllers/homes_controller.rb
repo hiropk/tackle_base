b4b326d02285889e1bc473d5008a178582141f72
HiroyukiKano <kano@netlab.jp>
2025-04-02 13:49:05 +0900

    ロッド一覧の埋め込み

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
   def index
+    @search = Rod.ransack(params[:q])
+    @search.sorts = "id desc" if @search.sorts.empty?
+    @rods = @search.result.page(params[:page])
   end
 end

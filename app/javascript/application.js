ae6df9fc1744f00b22d519e6bbeb0a1bc795bde5
HiroyukiKano <kano@netlab.jp>
2025-04-08 17:26:55 +0900

    検索の修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,53 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails";
+
+function handleHamburgerMenu() {
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const mobileNav = document.getElementById("mobile-nav");
+  const menuLinks = document.querySelectorAll(".mobile-nav ul li a");
+
+  hamburgerMenu.addEventListener("click", (event) => {
+    event.stopPropagation();
+    mobileNav.classList.toggle("show");
+  });
+
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileNav.classList.remove("show");
+    });
+  });
+
+  document.addEventListener("click", (event) => {
+    if (
+      !mobileNav.contains(event.target) &&
+      !hamburgerMenu.contains(event.target)
+    ) {
+      mobileNav.classList.remove("show");
+    }
+  });
+}
+
+function toggleRodSearchForm() {
+  const toggleButton = document.getElementById("toggle_search_form");
+  const searchForm = document.getElementById("rod_search_form");
+
+  toggleButton.addEventListener("click", function () {
+    if (searchForm.classList.contains("hidden")) {
+      searchForm.classList.remove("hidden");
+      toggleButton.textContent = "検索フォームOFF";
+    } else {
+      searchForm.classList.add("hidden");
+      toggleButton.textContent = "検索フォームON";
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  handleHamburgerMenu();
+  toggleRodSearchForm();
+});
+
+document.addEventListener("turbo:frame-load", function () {
+  handleHamburgerMenu();
+  toggleRodSearchForm();
+});

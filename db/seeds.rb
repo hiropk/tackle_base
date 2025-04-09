5cd61d8ecc8cd35188c2f0a60110fca7d37410c2
HiroyukiKano <kano@netlab.jp>
2025-04-09 22:39:55 +0900

    seedの更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ end
 
 rods = []
 100.times do |i|
-  rods << { user: admin, name: "ロッド#{i+1}", brand: "ブランド#{i+1}", length: 6.5, fishing_type: 0, power: 0, reel_type: 0, min_weight: 100, max_weight: 200, purchase_date: Date.current,  notes: "メモ#{i+1}" }
+  rods << { user: admin, name: "ロッド#{i+1}", brand: "ブランド#{i+1}", length: 6.5, fishing_type: 0, power: 0, reel_type: 0, min_weight: 100, max_weight: 200, purchase_date: Date.current, price: 12345,  notes: "メモ#{i+1}" }
 end
 
 Rod.create(rods)

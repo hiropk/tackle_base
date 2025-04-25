7cefc5d69be2db7c38fd5e3db520d4a8e8a5b55c
HiroyukiKano <kano@netlab.jp>
2025-04-25 17:22:15 +0900

    カラム名変更の適用

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -38,6 +38,6 @@ class ProfilesController < ApplicationController
   end
 
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :residence, fishing_area: [], interest_fishing: [])
+    params.require(:profile).permit(:first_name, :last_name, :residence, fishing_areas: [], interest_fishings: [])
   end
 end

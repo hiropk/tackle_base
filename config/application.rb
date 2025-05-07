a7bbe195cac44f0769b0cfe789ea4a7f0b7da08b
HiroyukiKano <kano@netlab.jp>
2025-05-07 21:18:03 +0900

    デプロイ下準備

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module FamiliarFishingManagement
     #
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
 
     config.action_mailer.default_options = { content_type: "text/html" }

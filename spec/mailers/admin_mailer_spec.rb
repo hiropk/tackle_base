dc95e8d21e11fbcf4ad7a4f1c89621778de7cd85
HiroyukiKano <kano@netlab.jp>
2025-05-15 10:35:36 +0900

    admin mailerのspecを追加

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -1,18 +1,31 @@
 require "rails_helper"
 
 RSpec.describe AdminMailer, type: :mailer do
-  describe "admin_message" do
-    let(:mail) { AdminMailer.admin_message }
+  describe "individual_message" do
+    let(:user) { create(:user, email_address: "user@example.com") }
+    let(:admin_user) { create(:user, email_address: "admin@example.com", is_admin: true) }
+    let(:subject) { "テストメール" }
+    let(:body) { "これはテストメールです。" }
+    let(:mail) { AdminMailer.individual_message(user, subject, body, admin_user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Admin message")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq(subject)
+      expect(mail.to).to eq([ user.email_address ])
+      expect(mail.from).to eq([ admin_user.email_address ])
+      expect(mail[:from].value).to eq("タックルベース管理者 <#{admin_user.email_address}>")
+      expect(mail.bcc).to eq([ admin_user.email_address ])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders the body in both text and HTML formats" do
+      expect(mail.text_part.body.encoded).to include(body)
+      expect(mail.html_part.body.encoded).to include(body)
+    end
+
+    it "includes admin user's signature in both formats" do
+      [ "加納 寛之", "遊漁船ファミリア 船長", "タックルベース 開発者" ].each do |text|
+        expect(mail.text_part.body.encoded).to include(text)
+        expect(mail.html_part.body.encoded).to include(text)
+      end
     end
   end
-
 end

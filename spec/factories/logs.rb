339fed4b385705329337e5dbd223518c17c100cd
HiroyukiKano <kano@netlab.jp>
2025-05-28 14:56:34 +0900

    factoryの修正

--- spec/factories/logs.rb
+++ spec/factories/logs.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :log do
     user
     fishing_date { Date.current }
-    start_time { Time.current }
     end_time { Time.current + 4.hours }
     area { "Tokyo Bay" }
     fishing_guide_boat { false }
